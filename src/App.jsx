6fa790bf93e2f788571033c2228fc3cb296478cd
Frempong <samankwaa.frempong@gmail.com>
2024-01-19 19:25:03 +0000

    Update missing logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-// import ezImage from '../assets/ez-image.png'/
+import ezImage from './assets/ez-image.png'
 
 function App() {
 
   return (
     <>
         <div>
-          <img src="src\assets\ez-image.png" alt="topp image" className="top-image" />
+          <img src={ezImage} alt="topp image" className="top-image" />
           <h1 className="notice">
             OUR MENU WILL BE AVAILABLE SOON
           </h1>
