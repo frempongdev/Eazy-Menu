f5717a1668fee2d1d20934b7c7c255b8ce928c70
Frempong <samankwaa.frempong@gmail.com>
2024-01-19 18:53:42 +0000

    initial Menu page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+// import ezImage from '../assets/ez-image.png'/
+
+function App() {
+
+  return (
+    <>
+        <div>
+          <img src="src\assets\ez-image.png" alt="topp image" className="top-image" />
+          <h1 className="notice">
+            OUR MENU WILL BE AVAILABLE SOON
+          </h1>
+        </div>
+    </>
+  )
+}
+
+export default App
