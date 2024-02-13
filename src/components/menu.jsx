e111894b413f5b6105f449d69dc6ec03581df751
Frempong <samankwaa.frempong@gmail.com>
2024-02-13 14:18:16 +0000

    Add Menu component

---
+++ src/components/menu.jsx
@@ -0,0 +1,59 @@
+import '../styles/menu.css'
+
+const Menu = () => {
+  return (
+    <div className="menu-wrapper">
+      <h2>MENU</h2>
+      <div className="menus-box">
+        <div className="stews">
+          <div className="image-container">
+            <div className="image-circle">
+              <img src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="stew-image" />
+            </div>
+          </div>
+          <h4 className="title-menu">Rice Meals</h4>
+          <div className="stew-list">
+            <p className="stew-item">+ Garden Eggs Stew</p>
+            <p className="stew-item">+ Palava Sauce</p>
+            <p className="stew-item">+ Beans Stew</p>
+            <p className="stew-item">+ Egg Stew</p>
+            <p className="stew-item">+ Tomato Sauce</p>
+          </div>
+        </div>
+        <div className="stews">
+          <div className="image-container">
+          <div className="image-circle">
+            <img src="https://images.unsplash.com/photo-1561043433-b0bb121aacef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="stew-image" />
+          </div>
+          </div>
+          <h4 className="title-menu">Yam & Plantain</h4>
+          <div className="stew-list">
+            <p className="stew-item">+ Garden Eggs Stew</p>
+            <p className="stew-item">+ Palava Sauce</p>
+            <p className="stew-item">+ Beans Stew</p>
+            <p className="stew-item">+ Egg Stew</p>
+            <p className="stew-item">+ Tomato Sauce</p>
+          </div>
+        </div>
+        <div className="stews">
+          <div className="image-container">
+          <div className="image-circle">
+            <img src="https://images.unsplash.com/photo-1584689070416-6fa78dd07fc0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="stew-image" />
+          </div>
+          </div>
+          <h4 className="title-menu">Others</h4>
+          <div className="stew-list">
+            <p className="stew-item">Fufu ( Sunday Special )</p>
+            <p className="stew-item">Random Saturdays</p>
+            {/* <p className="stew-item">+ Beans Stew</p>
+            <p className="stew-item">+ Egg Stew</p>
+            <p className="stew-item">+ Tomato Sauce</p> */}
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Menu
