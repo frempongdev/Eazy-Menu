12ddc74d8fcb1eee9af994e88c476122cc25f95c
Frempong <samankwaa.frempong@gmail.com>
2024-02-13 15:39:50 +0000

    Adjust style for Top.jsx for responsive design

--- src/components/top.jsx
+++ src/components/top.jsx
@@ -7,7 +7,9 @@ const Top = () => {
         <div className="logo-line">
           <img src={ezImage} alt="topp image" className="top-image" />
           <div className="contact">
-            <p className="order">ODRER NOW</p>
+            <div className="order-box">
+              <p className="order">ORDER NOW</p>
+            </div>
             <div className="numbers">
               <a href='tel:0201875762' className="number"><h2>0201875762</h2></a>
               <a href='tel:0241689066' className="number"><h2>0241689066</h2></a>
