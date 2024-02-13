9e482d947d3e22eab1f4e9969e5a9be44714dcbe
Frempong <samankwaa.frempong@gmail.com>
2024-02-13 14:19:22 +0000

    Add all Top and Menu components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css'
-import ezImage from './assets/ez-image.png'
+// import ezImage from './assets/ez-image.png'
+import Menu from './components/menu'
+import Top from './components/top'
 
 function App() {
 
   return (
     <>
-        <div>
+        {/* <div>
           <img src={ezImage} alt="topp image" className="top-image" />
           <h1 className="notice">
             OUR MENU WILL BE AVAILABLE SOON
           </h1>
-        </div>
+        </div> */}
+        <Top />
+        <Menu />
     </>
   )
 }
