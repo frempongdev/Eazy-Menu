5ba3d0b9bf27670ffe237d937ff118e26a7df9a4
Frempong <samankwaa.frempong@gmail.com>
2024-02-20 14:26:16 +0000

    Change rice image

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -8,7 +8,7 @@ const Menu = () => {
         <div className="stews">
           <div className="image-container">
             <div className="image-circle">
-              <img src="https://images.unsplash.com/photo-1599354607459-81c8b0d90bf5?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="stew-image" />
+              <img src="https://images.unsplash.com/photo-1569058242252-623df46b5025?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="stew-image" />
             </div>
           </div>
           <h4 className="title-menu">Rice Meals</h4>
