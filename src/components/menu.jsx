cabba64835934a46b472e0fea412e6dd4069be4f
Frempong <samankwaa.frempong@gmail.com>
2024-02-20 13:44:37 +0000

    Add Yam chips

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -8,7 +8,7 @@ const Menu = () => {
         <div className="stews">
           <div className="image-container">
             <div className="image-circle">
-              <img src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="stew-image" />
+              <img src="https://images.unsplash.com/photo-1599354607459-81c8b0d90bf5?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="stew-image" />
             </div>
           </div>
           <h4 className="title-menu">Rice Meals</h4>
@@ -43,7 +43,8 @@ const Menu = () => {
           </div>
           <h4 className="title-menu">Others</h4>
           <div className="stew-list">
-            <p className="stew-item">Jollof Rice and Chicken</p>
+            <p className="stew-item">Jollof Rice + Chicken</p>
+            <p className="stew-item">Yam Chips + Chicken Wings</p>
             <p className="stew-item">Fufu ( Sunday Special )</p>
             <p className="stew-item">Random Saturdays</p>
             {/* <p className="stew-item">+ Beans Stew</p>
